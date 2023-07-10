bbc10951352fe2b6ea9f52c1ff6284473f6c4109
meghasyam1997 <meghasyam01@gmail.com>
2023-07-10 12:30:11 +0530

    creating auto_scaling_group and launch_template

---
+++ vars.tf
@@ -0,0 +1,14 @@
+variable "name" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "instance_class" {}
+variable "instance_count" {}
+variable "port" {}
+
+variable "subnets" {}
+variable "allow_db_cidr" {}
+
+variable "tags"{}
+variable "env" {}
+variable "vpc_id" {}
+variable "kms_arn" {}
